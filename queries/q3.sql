df2dcb299a5926d26bcb5a67147b893b44ef79ba
eugene-mindset <eugenemindset@gmail.com>
2021-04-10 16:20:34 -0400

    complete phase C, start phase D

---
+++ queries/q3.sql
@@ -0,0 +1,15 @@
+WITH SponsorCount AS (
+    SELECT member_id, congress, count(member_id) as num_bills
+    FROM Sponsor
+    GROUP BY member_id),
+HouseCount AS (
+    SELECT member_id, num_bills, party, state, district
+    FROM SponsorCount JOIN Role USING(member_id, congress)
+    WHERE chamber LIKE 'senate'
+), MaxHouseCount AS (
+    SELECT MAX(num_bills) as most_bills
+    FROM HouseCount
+)
+SELECT member_id, firstName, middleName, lastName, num_bills, party, state, district
+FROM (Member JOIN HouseCount USING (member_id)) JOIN MaxHouseCount
+where num_bills = most_bills;
