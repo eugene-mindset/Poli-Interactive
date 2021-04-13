af5d9806132d7020850f0fe9646a89061310ce12
Anderson Adon <andersonadon2@gmail.com>
2021-04-13 01:42:35 -0400

    Update q13.sql

--- queries/q13.sql
+++ queries/q13.sql
@@ -1,10 +1,19 @@
-SELECT party, MAX(counts)
-FROM
+-- Which party has the most bills passed that were sponsored by a member of that party?
+WITH PassedBills AS
 (
-    SELECT party, COUNT(party) AS counts
+    SELECT party, COUNT(party) AS numPassed
     FROM Sponsor
     NATURAL JOIN Bill
     NATURAL JOIN Role
     WHERE enacted="Yes"
     GROUP BY party
+),
+MaxPassed AS
+(
+    SELECT MAX(numPassed) as numPassed
+    FROM PassedBills
 )
+SELECT PassedBills.party, MaxPassed.numPassed
+FROM PassedBills
+JOIN MaxPassed
+ON PassedBills.numPassed = MaxPassed.numPassed
