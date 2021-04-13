e1fb6dde9625174fcdd5e1f347478d2004c97741
Anderson Adon <andersonadon2@gmail.com>
2021-04-13 01:36:33 -0400

    Update q11.sql

--- queries/q11.sql
+++ queries/q11.sql
@@ -1,12 +1,21 @@
-SELECT area, MAX(count)
-FROM
+-- What bill area appears most often in passed bills?
+WITH AreaCounts AS
 (
-    SELECT area, COUNT(area) as count
+    SELECT a.area, COUNT(a.area) as occurences
     FROM
     (
         SELECT bill_num, congress, area, enacted
         FROM Bill
         WHERE enacted="Yes"
-    )
+    ) AS a
     GROUP BY area
+),
+MaxCounts AS
+(
+    SELECT MAX(occurences) as occurences
+    FROM AreaCounts
 )
+SELECT a.area, b.occurences
+FROM AreaCounts AS a
+JOIN MaxCounts AS b
+ON a.occurences=b.occurences
