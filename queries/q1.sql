d5cbadf20554343cc04aa7a97ea46f78f1546b57
Anderson Adon <andersonadon2@gmail.com>
2021-04-13 01:28:07 -0400

    Update q1.sql

--- queries/q1.sql
+++ queries/q1.sql
@@ -1,13 +1,15 @@
+-- What bill(s) passed with the most amount of "Yes" votes in the senate,
+-- how many votes did it receive?
 SELECT * FROM
 (
-    SELECT bill_num, congress, MAX(yeses)
+    SELECT bill_num, congress, MAX(numYes) as numYes
     FROM
     (
-        SELECT bill_num, congress, COUNT(position) as yeses
+        SELECT bill_num, congress, COUNT(position) as numYes
         FROM Vote
         NATURAL JOIN Role
         WHERE chamber="senate" AND position="Yes"
         GROUP BY bill_num, congress
-    )
-)
+    ) AS a
+) AS a
 NATURAL JOIN Bill;
